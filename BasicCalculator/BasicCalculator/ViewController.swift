01c1d622ed6ab32b2d78dd7ddb91d020f789383f
Raiyan Sharif <raiyan.sharif@bjitgroup.com>
2023-10-13 12:25:41 +0600

    calculator basic

--- BasicCalculator/BasicCalculator/ViewController.swift
+++ BasicCalculator/BasicCalculator/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func numberPressed(_ sender: UIButton) {
+        
+        if resultLabel.text == "0"{
+            resultLabel.text = "\(sender.tag)"
+            print(sender.tag)
+        }else{
+            resultLabel.text = resultLabel.text! + "\(sender.tag)"
+            print(sender.tag)
+        }
+    }
 
+    @IBAction func addButtonPressed(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "+"
+    }
+    
+    @IBAction func subtractionButtonPressed(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "-"
+    }
+    @IBAction func multiplicationButtonPressed(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "*"
+    }
+
+    @IBAction func divisionButtonPressed(_ sender: UIButton) {
+        resultLabel.text = resultLabel.text! + "/"
+    }
+
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        resultLabel.text = ""
+    }
+    @IBAction func evaluteButtonPressed(_ sender: UIButton) {
+
+    }
 }
 
